81074afbea6d42ba85c4725d532b26c760bd3108
Amr Adel <amradelkhalil@gmail.com>
2019-10-19 14:49:03 +0200

    fix submitting more than one time

--- app/services/movie_services/add_rate.rb
+++ app/services/movie_services/add_rate.rb
@@ -1,7 +1,12 @@
 module MovieServices
   class AddRate
     def self.call(user, rating_params)
-      Rating.create(user_id: user.id, movie_id: rating_params[:movie_id], rate: rating_params[:rate])
+      rate = Rating.where(user_id: user.id, movie_id: rating_params[:movie_id]).first
+      if rate.nil?
+        Rating.create(user_id: user.id, movie_id: rating_params[:movie_id], rate: rating_params[:rate])
+      else
+        rate.update(rate: rating_params[:rate])
+      end
       MovieServices::RatingInfo.call(rating_params[:movie_id])
     end
   end
