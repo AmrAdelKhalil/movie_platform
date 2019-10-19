3392a314bd751c49ca0a23a0cb5a889547a807a4
Amr Adel <amradelkhalil@gmail.com>
2019-10-19 14:40:55 +0200

    Add rails-admin gem, Update configuration of the rails admin, Add admin field to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :ratings
-  has_many :movies_rated, through: :ratings, class_name: 'Movie'
+  has_many :movies_rated, through: :ratings, source: :movie
   has_and_belongs_to_many :watchlist, join_table: :movies_users, class_name: 'Movie'
 
   def self.new_with_session(params, session)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
   get 'movies/add_review'
   get 'movies/rating_info'
   get 'movies/watchlist_it'
@@ -8,5 +12,6 @@ Rails.application.routes.draw do
   get 'newses/index'
   get 'genres/movies_with_genre'
   get 'movies/open_this_week'
+  # get 'dashboards/index'
   root 'dashboards#index'
 end
