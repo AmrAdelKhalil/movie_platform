6b3ce1208d6e31885a06b8b79d5c7f04a229377a
Amr Adel <amradelkhalil@gmail.com>
2019-10-20 12:39:09 +0200

    Add new concern for Omniauth

---
+++ app/models/concerns/omniauthable.rb
@@ -0,0 +1,21 @@
+module Omniauthable
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
+
+    def from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name
+      end
+    end
+  end
+end
